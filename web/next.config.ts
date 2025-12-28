32819363b1efee7498a7ce2e260d0582e147ae87
Markus Thiede <mailtothemaru@gmail.com>
2025-12-28 16:21:39 +0100

    use standalone next build for smaller docker image size

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'standalone',
   // async rewrites() {
   //   if (process.env.NODE_ENV === 'development') {
   //     return [
